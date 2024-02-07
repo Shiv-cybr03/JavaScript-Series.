e7eafb2812ac32999440e2d099a610b1bd21b977
Shiv yadav <yshiv4074@gmail.com>
2024-02-07 20:21:32 +0530

    days-01-complete

---
+++ Days-01/string.js
@@ -0,0 +1,32 @@
+const name = "shivkumar"
+const repo = 500
+
+console.log(name + repo);   // this is not a better 
+
+const gameName = "gilidanta"
+console.log(gameName.length);
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.slice(0,5));
+
+const newName = gameName.substring(0,8)
+console.log(newName);
+
+const newString = "    shiv  "
+console.log(newString);
+console.log(newString.trim()); //to remove extra space on string 
+
+const myName = "shibu"
+console.log(myName.replace('b','v'));  // to replace to one word or sentence to another word or sentence 
+
+let director = "rajamoli"
+console.log(director.includes('o'));   //to chech words is present or not:
+
+let str = "appple, banana, mango"
+console.log(str.substring(0,15));  
+
+let stName = "arjit "
+console.log(stName.repeat(5));     //output is 5 times name repeat using this methods
+console.log(stName.search('j'));    // to search element is present or not
+
+let example = "ritik 'roshan' "
+console.log(example);              // string declared template or methods
---
+++ Days-01/variable&dataType.js
@@ -0,0 +1,63 @@
+//**************Variables**************/
+
+console.log("shiv")
+
+//how to declared variable:
+const accountId = 110
+let accountEmail = "shivydv@gmail.com"
+var accountNo = 150 //plz var is not use instaly:
+userstate = "jaipur" //This is not for better methods for declared for variable;
+
+let UserNmae;
+
+accountEmail = "shivram@gmail.com"
+accountNo = 500
+userstate= "delhi"
+
+console.log([accountId,accountEmail,accountNo,userstate,UserNmae]);
+
+
+//****************Data Type*******************************
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high.
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined datatype
+console.log(typeof null); // object datatype.
+
+let score = "shiv"
+console.log(typeof(score));    // String datatype.
+
+let valueInNumber=Number(score)   // this line score convert Number datatypes.
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+//conversation of datatypes.
+
+//"33" => 33 
+// "33abc" =>NaN
+// true => 1 : flase => 0
